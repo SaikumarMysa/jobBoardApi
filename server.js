7c364a66f3d71c91451f7e61721b4a26940c3456
SaiKumar <saikumarmysa31@gmail.com>
2024-06-26 19:09:02 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path:'./config.env'});
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose.connect(DB,{
+
+}).then(()=>{
+    console.log('Database connection successful')
+})
+const port = 5050;
+app.listen(port,(req,res)=>{
+    console.log(`Listening to server on port ${port}`)
+})
