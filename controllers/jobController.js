b308fc602f76c65ce17cd73d36f20132178c062f
SaiKumar <saikumarmysa31@gmail.com>
2024-07-19 13:19:47 +0530

    initial commit

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -37,51 +37,87 @@ exports.createJob = catchAsync(async(req,res,next) =>{
 
 //showJobs
 exports.showAllJobs = catchAsync(async(req,res,next) =>{
-        // console.log(req.query);
+
         //Building query
+
         const queryObj = {...req.query};
-          const excludedFields = ['page', 'sort', 'limit', 'fields'];
-          excludedFields.forEach(el => delete queryObj[el]);
+
+
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+        excludedFields.forEach(el => delete queryObj[el]);
+
         //1.filtering
-         let queryStr=JSON.stringify(queryObj);
-         queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`);
+
+         let queryStr = JSON.stringify(queryObj);
+
+         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match=>`$${match}`);
         
         let query = Job.find(JSON.parse(queryStr));
+        
 
         //2.sorting
+
         if(req.query.sort){
-            // query = query.sort(req.query.sort)
+
             const sortBy = req.query.sort.split(',').join(' ')
+
             query = query.sort(sortBy);
+
         }else{
+
             query = query.sort('-createdAt')
         }
 
         //3.Limiting fields:allowing the clients which fields they want to see in response
+
         if(req.query.fields){
+            
+
             const fields = req.query.fields.split(',').join(' ')
+            
             query = query.select(fields)
+
         }else{
+
             query = query.select('-__v')
         }
 
         //4.pagination
-        const page = req.query.page *1 ||1;
+
+        const page = req.query.page *1 || 1;
+
         const limit = req.query.limit * 1 || 10;
+
         const skip = (page -1)* limit;
+
         if(req.query.page){
+
+            //console.log(req.query)
+
             query = query.skip(skip).limit(limit);
+
         }
+
         //executing query
+
         const jobs  = await query;
+
         res.status(200).json({
+
             status:'success',
+
             results:jobs.length,
+
             data:{
+
                 jobs
             }
+
         }) 
-    next();   
+
+    next();  
+
 })
 
 //deleteJob
